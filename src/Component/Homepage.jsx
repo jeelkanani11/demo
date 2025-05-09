a72dda36dc9db764e3c35f23fd73be68b17634ad
jeel <jeelkanani11@gmail.com>
2025-05-09 09:45:53 +0530

    first commit

---
+++ src/Component/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+      hello
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi id sequi expedita aspernatur officiis culpa sed suscipit, magni, ullam quas repellat voluptatibus blanditiis facere sapiente, in amet excepturi doloribus!
+    </div>
+  )
+}
+
+export default Homepage
