87ca374bc1917b3ea98381e220de8e46f4d8b836
jeel <jeelkanani11@gmail.com>
2025-05-09 16:52:55 +0530

    third  commit

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -4,6 +4,7 @@ function About() {
   return (
     <div>
       aboput page
+      <div style={{color:"red"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti ea eligendi, placeat odit quaerat nesciunt! Rerum quisquam voluptates atque aliquam fugiat debitis quasi ad, quod, maiores ipsam quas sapiente veritatis.</div>
     </div>
   )
 }
